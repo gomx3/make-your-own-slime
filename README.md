2c994c49cad24d5c6eafa1478f49dd9c170bceb3
Sowon Kim <sso626262@naver.com>
2025-03-01 20:16:46 +0900

    add main

---
+++ README.md
@@ -0,0 +1,3 @@
+## STUDY-THREEJS
+
+https://threejs.org/docs/index.html#manual/ko/introduction/Creating-a-scene
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,10 @@
+import { Object } from '../components/main/object'
+
 export default function StudyPage() {
     return (
-        <>
+        <div className="flex flex-col items-center">
             <div>main page</div>
-        </>
+            <Object />
+        </div>
     )
 }
