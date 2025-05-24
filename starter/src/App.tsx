75b882e53249b284489362da00685cb4a31997c7
Sowon Kim <sso626262@naver.com>
2025-05-25 00:37:40 +0900

    add footer

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -2,15 +2,19 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Blob from './components/Blob'
 import Controller from './components/Controller'
 import { system } from '@chakra-ui/react/preset'
+import Footer from './components/_common/footer'
 
 function App() {
     return (
         <ChakraProvider value={system}>
-            <main className="flex flex-col justify-center items-center">
-                <h1 className="text-xl">make your own slime **</h1>
-                <Blob />
-                <Controller />
-            </main>
+            <div className="flex flex-col min-h-screen">
+                <main className="flex flex-1 flex-col justify-center items-center">
+                    <h1 className="text-xl">make your own slime **</h1>
+                    <Blob />
+                    <Controller />
+                </main>
+                <Footer />
+            </div>
         </ChakraProvider>
     )
 }
---
+++ starter/src/components/_common/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex justify-end bg-gray-50">
+            <p className="p-3">곰 공부용</p>
+        </footer>
+    )
+}
